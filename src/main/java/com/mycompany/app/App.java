b9a736ffb090f737450c45e8214e46e35117ac99
brillkuo <137852227+brillkuo@users.noreply.github.com>
2025-06-06 16:36:45 +0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World brill!";
+    private static final String MESSAGE = "Hello World brill0606!";
 
     public App() {}
 
