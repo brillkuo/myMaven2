89638b2369ec10e4b3da95c5cb8ee2bbee53bed0
brillkuo <137852227+brillkuo@users.noreply.github.com>
2025-06-16 15:54:05 +0800

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World0606!", app.getMessage());
+        assertEquals("Hello World brill0606!", app.getMessage());
     }
 }
